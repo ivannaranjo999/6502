d5d65b82dee7046bd52131c47f53ce119f90e850
ivannaranjo999 <ivanlink99@gmail.com>
2025-08-10 19:09:19 +0200

    New instructions related to LDA

--- m6502.cpp
+++ m6502.cpp
@@ -35,6 +35,8 @@ namespace m6502{
 
   using u32 = unsigned int;
 
+  u32 cycles;
+
   struct StatusFlags
   {
     byte C : 1;       // 0: Carry Flag
@@ -50,6 +52,9 @@ namespace m6502{
   enum Opcode : byte {
     LDA_IMMEDIATE = 0xA9,
     LDA_ZEROPAGE  = 0xA5,
+    LDA_ZEROPAGEX = 0xB5,
+    LDA_ABSOLUTE  = 0xAD,
+    LDA_ABSOLUTEX = 0xBD,
   };
 
   enum class LogLevel : int {
@@ -91,8 +96,8 @@ namespace m6502{
     static constexpr u32 MAX_MEM = 1024 * 64;  // 64 KB
     byte data[MAX_MEM];
 
-    byte operator[](word addr) const { return data [addr]; }
-    byte& operator[](word addr) {return data[addr]; }
+    byte operator[](word addr) const { ++cycles; return data [addr]; }
+    byte& operator[](word addr) { ++cycles; return data[addr]; }
 
     void clear() {
       for (u32 i = 0; i < MAX_MEM; ++i)
@@ -122,6 +127,7 @@ namespace m6502{
       StatusFlags Flag;   // Individual flags
     } P;
 
+
     void reset(MEM& mem) {
       mem.clear();
       mem.setResetVector();
@@ -129,11 +135,15 @@ namespace m6502{
       SP = 0xFD;
       A = Y = X = 0;
       P.PS = 0b00100100;
+      cycles = 0;
 
       log(LogLevel::Info,"CPU resetted\n");
     }
 
     byte fetchByte(const MEM& mem) {
+      log(LogLevel::Info, "Fetched 0x%02x from mem[0x%04x]\n", mem[PC],PC); 
+      --cycles; // Remove cycle counted in log call 
+
       return mem[PC++];
     }
 
@@ -144,7 +154,7 @@ namespace m6502{
 
     void execInstruction(const MEM& mem) {
       byte opcode = fetchByte(mem);
-
+      
       switch(opcode) {
         case LDA_IMMEDIATE: {
           log(LogLevel::Info,"LDA Immediate\n");
@@ -157,6 +167,32 @@ namespace m6502{
           log(LogLevel::Info,"LDA ZP\n");
           byte zpAddr = fetchByte(mem);
           A = mem[zpAddr];
+          setZN(A);
+        } break;
+
+        case LDA_ZEROPAGEX: {
+          log(LogLevel::Info,"LDA ZP X\n");
+          byte zpAddr = fetchByte(mem);
+          zpAddr = (zpAddr + X) & 0xFF;
+          A = mem[zpAddr];
+          setZN(A);
+        } break;
+
+        case LDA_ABSOLUTE: {
+          log(LogLevel::Info,"LDA Absolute\n");
+          byte lowAddr = fetchByte(mem);
+          byte highAddr = fetchByte(mem);
+          word addr = lowAddr | (highAddr << 8);
+          A = mem[addr];
+          setZN(A);
+        } break;
+
+        case LDA_ABSOLUTEX: {
+          log(LogLevel::Info,"LDA Absolute X\n");
+          byte lowAddr = fetchByte(mem);
+          byte highAddr = fetchByte(mem);
+          word addr = (lowAddr | (highAddr << 8)) + X;
+          A = mem[addr];
           setZN(A);
         } break;
 
@@ -181,10 +217,40 @@ int main()
   mem[0x8001] = 0x55; // Byte to put in A
   */
   /* Code 2: LDA Zero Page */
+  /*
   mem[0x8000] = 0xA5; // Instruction
-  mem[0x8001] = 0x02; // Position in Zero Page
+  mem[0x8001] = 0x02; // Addr in Zero Page
   mem[0x0002] = 0x15; // Byte to put in A
+  */
+  // Code 3: LDA Zero Page X
+  /*
+  cpu.X = 0x10; //LDX not implemented yet
+  mem[0x8000] = 0xB5; // Instruction
+  mem[0x8001] = 0xFB; // Addr at ZP to be added with rX
+  mem[0x000B] = 0x12; // Byte to put in A
+  */
+  // Code 4: LDA Absolute
+  /*
+  mem[0x8000] = 0xAD; // Instruction
+  mem[0x8001] = 0x02; // Addr 0x0102
+  mem[0x8002] = 0x01;
+  mem[0x0102] = 0xAB; //Byte to put in A
+  */
+  // Code 5: LDA Absolute X
+  cpu.X = 0x10; //LDX not implemented yet
+  mem[0x8000] = 0xBD; // Instruction
+  mem[0x8001] = 0x02; // Addr 0x0102
+  mem[0x8002] = 0x01;
+  mem[0x0112] = 0xAB; //Byte to put in A
   /* END OF EMBEDDED CODE */ 
+
+  m6502::cycles = 0;
   cpu.execInstruction(mem);
-  m6502::log(m6502::LogLevel::Info,">> Accumulator: 0x%02X\n",cpu.A);
+
+  m6502::log(m6502::LogLevel::Info,"Final results:\n");
+  m6502::log(m6502::LogLevel::Info,"  PC -> 0x%04X\n",cpu.PC);
+  m6502::log(m6502::LogLevel::Info,"  Cy -> %u\n",m6502::cycles);
+  m6502::log(m6502::LogLevel::Info,"  Ac -> 0x%02X\n",cpu.A);
+  m6502::log(m6502::LogLevel::Info,"  rX -> 0x%02X\n",cpu.X);
+  m6502::log(m6502::LogLevel::Info,"  rY -> 0x%02X\n",cpu.Y);
 }
